7165c900a3a99e35e36c9aeb6966970f1635858b
jabae <jabae@princeton.edu>
2016-09-28 13:43:12 -0400

    Latest update

--- cell_info_typedef_gc.m
+++ cell_info_typedef_gc.m
@@ -29,38 +29,34 @@ struct('name','2i','annotation','','cells',[50004 17013 17092 20051 20234 20082 
 struct('name','2o','annotation','','cells',[17216 10005 10013 26062 26118]);
 % gallery id: 10005
 
-% minor stratum at layer 5
 struct('name','25','annotation','','cells',[20105 20104 20186 20237 20067 25006 17176 17132 20045 20036 26031 26134 26167 26042 26040 26037 26060 26066 26099 26117 26145 26175]);
 % gallery id: 17132
 
-% regular bilayer. more at off layer
 struct('name','27','annotation','','cells',[20117 17212 26051 26065]); 
 % gallery id: 26065
 
-% diffuse bilayer more at off layer
 struct('name','28','annotation','','cells',[20243 20155 20163 20257 26005 20167 26033]); 
 % gallery id: 20155
 
-% mini tOffAlpha
 struct('name','3i','annotation','','cells',[17135 20107 17077 26063 26104 26116 26188]);
 % gallery id: 17135
 
 struct('name','3o','annotation','','cells',[26155 20121 17037 17076]);
 % gallery id: 17037
 
-% purple 
+% On-Off DS caudal
 struct('name','37c','annotation','On-Off DS','cells',[26047 20002 20179 20210 20254 26101 20245 26137 26036 26056]); 
 % gallery id: 20210
 
-% green (+Z omni) ventral
+% On-Off DS dorsal
 struct('name','37d','annotation','On-Off DS','cells',[20014 20125 26029 26162 90002 26094]); 
 % gallery id: 20125
 
-% red 
+% On-Off DS rostral
 struct('name','37r','annotation','On-Off DS','cells',[90001 20213 25005 26103 17080 20220 26084 26165 26032]); 
 % gallery id: 20213
 
-% yellow dorsal
+% On-Off DS ventral
 struct('name','37v','annotation','On-Off DS','cells',[20016 20096 20233 26158 17161 20137 26115 26138 26178]); 
 % gallery id: 20233
 
@@ -71,6 +67,7 @@ struct('name','4i','annotation','','cells',[25004 17022 20170 17247 20174 17057 
 struct('name','4on','annotation','','cells',[20230 17064 17034 17167 17151 20041 26021 26086 26121 26146 26160]); 
 % gallery id: 20230
 
+% tOffalpha
 struct('name','4ow','annotation','tOff\alpha','cells',[17188 17079 20156 26004]); 
 % gallery id: 20156
  
@@ -80,18 +77,17 @@ struct('name','5si','annotation','','cells',[17040 20135 20183 17071 20070 17055
 struct('name','5so','annotation','','cells',[17160 17146 20223 17081 17127 17168 20053 20012 26046 17138 17011 26111 26140 26151 26181 26187]);
 % gallery id: 20223
 
-% W3 like
 struct('name','5ti','annotation','','cells',[17090 17181 50002 20114 20102 20216 20191 20184 20226 20262 20097 17093 17121 17159 20089 17190 17078 17059 20127 20055 26144 26170 26161 26156 26152 26123 26120 26112 26083 26053]);
 % gallery id: 20184
 
 struct('name','5to','annotation','','cells',[20128 20165 20240]); 
 % gallery id: 20240
 
-% W3: 
+% W3 
 struct('name','51','annotation','W3','cells',[20120 20212 20182 17098 17095 20153 20258 17035 20037 26177 26154 26136 26113 26098 26085 26054 26039 26025 26122]);
 % gallery id: 20212
 
-% OS. bistratified tall ~ tri-stratified
+% F-mini On
 struct('name','63','annotation','F-mini^{On}','cells',[20181 17140 20208 20178 17097 17114 17084 20140 20129 30003 20071 30002 20019 20011 20005 26057 26191 26148 26141 26125 26089 26068 26028 26027 26023]);
 % gallery id: 20178
 
@@ -107,38 +103,35 @@ struct('name','6sw','annotation','tOn\alpha','cells',[20222 20217 17083 20068 26
 struct('name','6t','annotation','F-midi^{On}','cells',[20255 20113 20232]); 
 % gallery id: 20232
 
-% On DSGC
+% On DS
 struct('name','7id','annotation','sOn DS','cells',[26070 26075 26078]); 
 % gallery id: 26078
 
-% On DSGC
+% On DS
 struct('name','7ir','annotation','sOn DS','cells',[20021 26002 26128 20075]); 
 % gallery id: 26002
 
-% On DSGC
+% On DS
 struct('name','7iv','annotation','sOn DS','cells',[17152 26077]); 
 % gallery id: 26077 
 
-% On DSGC
+% On DS
 struct('name','7o','annotation','tOn DS','cells',[17053 20180 26034 26048 26100 26130 20239]); 
 % gallery id: 20239
 
-% regular bilayer, layers further 
 struct('name','72','annotation','','cells',[20166 20221 17069 20074 26124]); 
 % gallery id: 20221
 
-% irregular bilayer
 struct('name','73','annotation','','cells',[20100 20187 20150 20043 26059 26073 26132]); 
 % gallery id: 20100
 
 struct('name','8n','annotation','','cells',[20126]); 
 % gallery id: 20126
 
-% on alpha sustained/M4
+% sOnalpha/M4
 struct('name','8w','annotation','sON\alpha','cells',[17111 26001 26071 26079]); 
 % gallery id: 17111
 
-% regular bilayer two layer even further
 struct('name','81i','annotation','','cells',[20158 26097 26090]); 
 % gallery id: 20158
 
@@ -155,21 +148,20 @@ struct('name','82wi','annotation','','cells',[20251 30001 26067]);
 struct('name','82wo','annotation','','cells',[20118 26091]);
 % gallery id: 20118
 
-% trimodal
 struct('name','85','annotation','','cells',[20063 20072 20200 17012 17038 26061 26045 26092 20046 26119]); 
 % gallery id: 20200
 
+% M5
 struct('name','9n','annotation','M5','cells',[26149 26168 26127 20006 20112 20076 20056 26074 26135]); 
 % gallery id: 20112
 
-% very wide 
+% M2
 struct('name','9w','annotation','M2','cells',[20228]); 
 % gallery id: 20228
 
 struct('name','91','annotation','M3','cells',[20218 25003 20042 26088 20081 20020]); 
 % gallery id: 20218
 
-% irregular bilayer 
 struct('name','915','annotation','','cells',[17009 20197]); 
 % gallery id: 17009
 
